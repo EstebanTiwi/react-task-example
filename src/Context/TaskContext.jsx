8fbe05e12420bf0484f44a520acbe9893006271b
EstebanTiwi <estebandeveloper6@gmail.com>
2022-10-16 03:12:22 -0500

    first commit

---
+++ src/Context/TaskContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useState, useEffect } from "react";
+import { tasks as data } from "../data/tasks";
+
+export const TaskContext = createContext();
+
+export function TaskContextProvider(props) {
+  const [tasks, setTasks] = useState([]);
+
+  function createTask(task) {
+    setTasks([
+      ...tasks,
+      {
+        title: task.title,
+        id: tasks.length,
+        description: task.description,
+      },
+    ]); // hazme una copia del array, y agregame uno que es el task
+  }
+
+  function deleteTask(taskId) {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  }
+
+  useEffect(() => {
+    setTasks(data);
+  }, []); // [] -> solo se va a ejecutar una vez cuando el cargue el componente y no más
+
+  return (
+    <TaskContext.Provider
+      value={{
+        tasks: tasks,
+        deleteTask: deleteTask,
+        createTask: createTask,
+      }}
+    >
+      {props.children}
+    </TaskContext.Provider>
+  );
+}
